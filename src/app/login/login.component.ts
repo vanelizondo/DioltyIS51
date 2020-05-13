3b22e543bacc2f33ce4869ccac0483453b839e9c
vanelizondo <vnelizondo89@mail.fresnostate.edu>
2020-05-13 00:42:26 -0700

    fixed login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,13 +13,22 @@ export interface IUser {
 })
 export class LoginComponent implements OnInit {
 
-  user: IUser = {username: '', password: ''};
+  user: IUser = { username: '', password: '' };
   constructor() { }
 
   ngOnInit() {
   }
 
   login(user: IUser) {
-    
+    const defaultUser: IUser = { username: '', password: '' };
+    if (user.username != null && user.password != null) {
+      if (user.username === defaultUser.username && user.username === defaultUser.password) {
+        // log the user in
+        // store user in localStorage
+        // navigate to contact page
+      }
+    } else {
+      // show error toast user
+    }
   }
 }
