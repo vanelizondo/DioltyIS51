a43dc61b6eacff84219215441e3083c4440abbd3
vanelizondo <vnelizondo89@mail.fresnostate.edu>
2020-05-13 23:11:33 -0700

    vanessa

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,34 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { InnerSubscriber } from 'rxjs/internal/InnerSubscriber';
+import { LocalStorageService } from '../localStorageService';
+import { Router } from '@angular/router';
+import { ToastService } from '../toast/toast.service';
 
 export interface IUser {
-  id?: number;
-  username: string;
-  password: string;
+    id?: number;
+    username: string;
+    password: string;
 }
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  user: IUser = { username: '', password: '' };
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  login(user: IUser) {
-    const defaultUser: IUser = { username: '', password: '' };
-    if (user.username != null && user.password != null) {
-      if (user.username === defaultUser.username && user.username === defaultUser.password) {
-        // log the user in
-        // store user in localStorage
-        // navigate to contact page
-      }
-    } else {
-      // show error toast user
+    user: IUser = { username: '', password: '' };
+    localStorageService: LocalStorageService<IUser>;
+    currentUser: IUser = null;
+    constructor(private router: Router, private toastService: ToastService) {
+        this.localStorageService = new LocalStorageService('user');
     }
-  }
+
+    ngOnInit() {
+        this.currentUser = this.localStorageService.getItemsFromLocalStorage();
+        console.log('this.currentuser.....', this.currentUser);
+        if (this.currentUser !== null) {
+            this.router.navigate(['clients']);
+
+        }
+    }
+
+    login(user: IUser) {
+        console.log(user);
+        const defaultUser: IUser = { username: 'bnelson', password: 'bnelson123' };
+        if (user.username !== '' && user.password !== '') {
+            if (user.username === defaultUser.username && user.password === defaultUser.password) {
+                // log the user in
+                // store user in local storage
+                this.localStorageService.saveItemsToLocalStorage(user);
+                // navigate to contacts page
+                this.router.navigate(['clients', user]);
+            } else {
+                // show error toast user
+                this.toastService.showToast('warning', 2000, 'Login failed! Please check your username or password.');
+            }
+        } else {
+            this.toastService.showToast('warning', 2000, 'Login failed! Please specify username and password.');
+        }
+    }
 }
